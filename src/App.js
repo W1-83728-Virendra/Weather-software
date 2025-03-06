2e553e5addb26d981fa8bd4bd98c1fd8257ae475
Virendra Deepak Mahamuni <162697836+W1-83728-Virendra@users.noreply.github.com>
2025-03-06 17:35:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,4 @@
-// import React from "react";
-// import Weather from "./Weather";
-// import "./Weather.css";
 
-// function App() {
-//   return (
-//     <div className="app">
-//       <h1>Weather App</h1>
-//       <Weather />
-//     </div>
-//   );
-// }
-
-// export default App;
 
 import React, { useState } from "react";
 import Weather from "./Weather";
